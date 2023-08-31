1ea75ab4a093a6aa7b159fff8291398b50e066b7
parksoohee <142369774+parksoohee@users.noreply.github.com>
2023-08-31 14:08:16 +0900

    Add files via upload

---
+++ js/insta.js
@@ -0,0 +1,72 @@
+$(function(){
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img1").mouseenter(function(){
+        $(".imgClass1").attr("src", "img/insta_hover1.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img1").mouseleave(function(){
+        $(".imgClass1").attr("src", "img/insta1.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img2").mouseenter(function(){
+        $(".imgClass2").attr("src", "img/insta_hover2.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img2").mouseleave(function(){
+        $(".imgClass2").attr("src", "img/insta2.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img3").mouseenter(function(){
+        $(".imgClass3").attr("src", "img/insta_hover3.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img3").mouseleave(function(){
+        $(".imgClass3").attr("src", "img/insta3.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img4").mouseenter(function(){
+        $(".imgClass4").attr("src", "img/insta_hover4.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img4").mouseleave(function(){
+        $(".imgClass4").attr("src", "img/insta4.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img5").mouseenter(function(){
+        $(".imgClass5").attr("src", "img/insta_hover5.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img5").mouseleave(function(){
+        $(".imgClass5").attr("src", "img/insta5.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img6").mouseenter(function(){
+        $(".imgClass6").attr("src", "img/insta_hover1.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img6").mouseleave(function(){
+        $(".imgClass6").attr("src", "img/insta1.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img7").mouseenter(function(){
+        $(".imgClass7").attr("src", "img/insta_hover2.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img7").mouseleave(function(){
+        $(".imgClass7").attr("src", "img/insta2.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img8").mouseenter(function(){
+        $(".imgClass8").attr("src", "img/insta_hover3.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img8").mouseleave(function(){
+        $(".imgClass8").attr("src", "img/insta3.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img9").mouseenter(function(){
+        $(".imgClass9").attr("src", "img/insta_hover4.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img9").mouseleave(function(){
+        $(".imgClass9").attr("src", "img/insta4.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+    //이미지 변경 함수 호출
+    $(".insta_slide_inner ul .img10").mouseenter(function(){
+        $(".imgClass10").attr("src", "img/insta_hover5.png");//변경될 이미지 주소를 넣어주세요
+    });
+    $(".insta_slide_inner ul .img10").mouseleave(function(){
+        $(".imgClass10").attr("src", "img/insta5.png");//변경되기 전 이미지 주소를 넣어주세요
+    });
+});
